5e6afad1a2b04aa99e4aea2893db138759ea05cb
kokou <magloirekombede796@gmail.com>
2023-08-09 17:39:15 +0200

    chap3, la syntaxe jsx

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-ReactDom(document.querySelector("#app"),{},"bonjour");
+let n=0;
+
+function render (){
+const title = React.createElement('h1',{},"bonjour les gens ",React.createElement('span',{},n.toString().padStart(4,"0")));
+ReactDOM.render(title,document.querySelector("#app"));
+
+}
+
+window.setInterval(()=>{
+    n++;
+    render();
+},1000);
