e7b38d65144866275119fd62d70e6734710e07b4
kokou <magloirekombede796@gmail.com>
2023-08-15 21:52:03 +0200

    partie 1

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-let n=0;
 
-function render (){
-const title = React.createElement('h1',{},"bonjour les gens ",React.createElement('span',{},n.toString().padStart(4,"0")));
-ReactDOM.render(title,document.querySelector("#app"));
+const title ="La maison jungle"
+function Header (){
+    return (<div>
+        <h1>{title.toUpperCase()}</h1>
+    </div>)
 
 }
+function Description() {
+    return (<p>Ici achetez toutes les plantes dont vous avez toujours rêvé 🌵🌱🎍</p>)
+}
+function Banner() {
+    return (<div>
+        <Header />
+        <Description />
+    </div>)
+}
+function Cart(){
+let p1=8;
+let p2=10;
+let p3=15;
+    return (
+    <div>
+    <h2>Panier</h2>
+    <ul>
+        <li> monstera: {p1}€</li>
+        <li> lierre : {p2}€</li>
+        <li>bouquet de fleurs : {p3}€</li>
+    </ul>
+    Total: {p1+p2+p3}€
+    </div>);
+}
 
-window.setInterval(()=>{
-    n++;
-    render();
-},1000);
+ReactDOM.render( <React.Fragment><Header/><Description/><Cart/></React.Fragment>,document.getElementById("root"));
+
